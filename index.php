f48341cea735631f200f9ac1ab9886e58123ebf8
Ulf Wilke <ulf.wilke@intellishop.ag>
2021-09-24 22:21:15 +0200

    added login/logout functionallity

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<?php
+session_start();
+
+// Including Database connection
+require_once "pdo.php";
+
+if (isset($_SESSION['test'])) {
+  echo $_SESSION['test'];
+}
+
+print_r ($_SESSION);
+
+
+?>
+
 <!doctype html>
 <html lang="de">
   <head>
@@ -7,11 +22,15 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-
+    <!-- custom css -->
+    <link href="style.css" rel="stylesheet">
     <title>Movie Tracker</title>
   </head>
   <body>
+    <div>
     <h1>Welcome to Movie Tracker</h1>
+
+ 
 
     <!-- Optional JavaScript; choose one of the two! -->
 
@@ -23,5 +42,39 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
     -->
+  </div>
+  <div>
+    <?php
+
+    if (!isset($_SESSION['name'])) {
+      
+      echo '<a href="login.php"><p>Please log in to use the application</p></a>';
+      die;
+    }
+    else {
+      echo '<a href="logout.php"><p>logout</p></a>';
+      
+    }
+    ?>
+    </div>
+    <div>My films</div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <card>Card1</card>
+        </div>
+        <div class="col">
+          Column
+        </div>
+        <div class="col">
+          Column
+        </div>
+      </div>
+    </div>
+    <form>
+      <input>search</input>
+    </form>
+
   </body>
 </html>
