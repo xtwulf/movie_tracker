0495460f76a60b1c6d7bcd464643f7d94a238600
Ulf Wilke <ulf.wilke@intellishop.ag>
2021-10-01 10:10:31 +0200

    added check on add.php when adding a film to favorites, now a check is done, if the film is already in favorites

--- add.php
+++ add.php
@@ -34,15 +34,19 @@ if ($_SESSION['film_info']['Poster'] == "" ) {
 // Check if year is valid and write in session variable
 $_SESSION['film_info']['year'] = checkYear($_SESSION['film_info']['year']); 
 
-echo('<pre>');
-echo("Session:");
-echo("<br>");
-print_r ($_SESSION);
 
+// Check if film already exists for this user in favorites table
+$film_id = $_SESSION['film_info']['film_id'];
+$user = $_SESSION['user_id'];
+$stmt = 'SELECT * FROM favorites WHERE imdbid = "'.$film_id.'" and user_id = '.$user;
+$test = $pdo->query($stmt)->fetchAll();
+if (!(empty($test))) {
+    $_SESSION['message'] = "<p class = \"alert\">\"".$_SESSION['film_info']['title'] . "\" ist bereits in den Favoriten gespeichert</p>";
+    header('location: index.php');
+    return;
+};
 
-
-
-
+// write film information into database
 $stmt = $pdo->prepare('INSERT INTO favorites (id, user_id, title, year, plot_short, imdbID, preview ) VALUES ( :id, :us_id, :title, :year, :plot_short, :imdbid, :preview )');
 $stmt->execute(array(
     ':id' => NULL,
